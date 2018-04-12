56191911ea0f166715bf7fbeef7759856bdb2c50
Hu Yongpeng <704725075@qq.com>
2018-04-12 18:05:08 +0800

    第一次修改

---
+++ src/main/java/com/hyp/test.java
@@ -0,0 +1,12 @@
+package com.hyp;
+
+
+/**
+ * Created by 70472 on 2018/4/12.
+ */
+public class test {
+    public static void main(String args[]){
+        System.out.println("HEllo maven!!");//第一次
+    }
+
+}
